dc51f0ec560b9d101e884e383bb2bcaeb426e335
Teresa Mayoral <temayoral@gmail.com>
2025-09-25 19:07:50 +0100

    create tables and drop tables queries

---
+++ db/queries/create_tables.js
@@ -0,0 +1,39 @@
+const db = require ("../connection");
+    
+async function createTables () {
+    await db.query(`CREATE TABLE property_types(
+        property_type VARCHAR(40) PRIMARY KEY NOT NULL,
+        description TEXT NOT NULL);`);
+
+    
+    await db.query(`CREATE TABLE users(
+        user_id SERIAL PRIMARY KEY,
+        first_name VARCHAR NOT NULL,
+        surname VARCHAR NOT NULL,
+        email VARCHAR NOT NULL,
+        phone_number VARCHAR NOT NULL,
+        is_host BOOL NOT NULL,
+        avatar VARCHAR,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);`);
+
+    
+    await db.query(`CREATE TABLE properties(
+        property_id SERIAL PRIMARY KEY,
+        host_id INT NOT NULL REFERENCES users(user_id),
+        name VARCHAR(100) NOT NULL,
+        location VARCHAR NOT NULL,
+        property_type VARCHAR(50) NOT NULL,
+        price_per_night DECIMAL NOT NULL,
+        description TEXT);`);
+
+    
+    await db.query(`CREATE TABLE reviews(
+        review_id SERIAL PRIMARY KEY,
+        property_id INT NOT NULL REFERENCES properties(property_id),
+        guest_id INT NOT NULL REFERENCES users(user_id),
+        rating INT NOT NULL,
+        comment TEXT,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);`);
+}
+
+module.exports = createTables
