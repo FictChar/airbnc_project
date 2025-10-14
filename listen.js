221e79216b4527a749a99ba44094f3e66ba4dd47
Teresa Mayoral <temayoral@gmail.com>
2025-10-14 18:38:42 +0100

    listen.js amended

--- listen.js
+++ listen.js
@@ -3,4 +3,5 @@ const app = require("./app");
 const {PORT = 9090} = process.env;
 
 app.listen(PORT, () => {
-    console.log(`Listening on ${PORT}...`)};
+    console.log(`Listening on ${PORT}...`);
+});
