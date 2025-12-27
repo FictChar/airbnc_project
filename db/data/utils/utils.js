e4003ca0ca54cd0dcbdb5afa9bc9c77fb93c3a9e
Teresa Mayoral <temayoral@gmail.com>
2025-12-27 13:42:57 +0000

    made a few change son how images were bwing inserted

--- db/data/utils/utils.js
+++ db/data/utils/utils.js
@@ -1,13 +1,8 @@
-const db = require("../../connection.js");
-const format = require('pg-format');
-const { usersData, propertiesData, reviewsData, imagesData } = require("../test/index.js");
-
-function usersLookUp (users) {
-return users.reduce((lookUp, user)=> {
-  const { first_name, surname, user_id } = user;
-  lookUp[`${first_name} ${surname}`] = user_id;
-  return lookUp;
-}, {});
+function usersLookUp(users) {
+  return users.reduce((lookUp, user) => {
+    lookUp[`${user.first_name} ${user.surname}`] = user.user_id;
+    return lookUp;
+  }, {});
 }
 
 function formatProperties(properties, userLookUp) {
@@ -18,26 +13,21 @@ function formatProperties(properties, userLookUp) {
     property_type, 
     price_per_night, 
     description 
-  }) => {
-    const host_id = userLookUp[host_name];
-    return [
-      host_id,   
-      name,
-      location,
-      property_type,
-      price_per_night,
-      description
-    ];
-  });
+  }) => [
+    userLookUp[host_name],
+    name,
+    location,
+    property_type,
+    price_per_night,
+    description
+  ]);
 }
 
-
 function propertiesLookUp(properties) {
-   return properties.reduce((lookUp, property)=>{
-   const { property_id, name } = property;
-  lookUp[name] = property_id;
-  return lookUp;
-}, {});
+  return properties.reduce((lookUp, property) => {
+    lookUp[property.name] = property.property_id;
+    return lookUp;
+  }, {});
 }
 
 function formatReviews(reviews, usersLookUpMap, propertiesLookUpMap) {
@@ -46,33 +36,40 @@ function formatReviews(reviews, usersLookUpMap, propertiesLookUpMap) {
     property_name, 
     rating, 
     comment, 
-    created_at }) => {
-    const property_id = propertiesLookUpMap[property_name];
-    const guest_id =  usersLookUpMap[guest_name];
-    return [
-      property_id,
-      guest_id,      
-      rating,
-      comment,
-      created_at
-    ];
-  });
+    created_at 
+  }) => [
+    propertiesLookUpMap[property_name],
+    usersLookUpMap[guest_name],
+    rating,
+    comment,
+    created_at
+  ]);
 }
 
 function createPropertyIdRef(properties) {
-  return properties.reduce((propertyLookUp, property) => {
-    const { name, property_id } = property;
-    propertyLookUp[name] = property_id;
-    return propertyLookUp;
+  return properties.reduce((ref, { name, property_id }) => {
+    ref[name] = property_id;
+    return ref;
   }, {});
 }
 
 function formattedImages(imagesData, propertyLookUp) {
-return imagesData.map(({ property_name, image_url, alt_tag }) => {
+  return imagesData.map(({ property_name, image_url, alt_tag }) => {
     const property_id = propertyLookUp[property_name];
+
+    if (!property_id) {
+      throw new Error(`No property found for image: ${property_name}`);
+    }
+
     return [property_id, image_url, alt_tag];
   });
 }
 
-
-module.exports = { usersLookUp, formatProperties, propertiesLookUp, formatReviews, createPropertyIdRef, formattedImages};
+module.exports = {
+  usersLookUp,
+  formatProperties,
+  propertiesLookUp,
+  formatReviews,
+  createPropertyIdRef,
+  formattedImages
+};
